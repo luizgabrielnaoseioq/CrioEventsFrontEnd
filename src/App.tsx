f36d0965e7b0d457239d064a00a0337bb9178297
Guilherme Pola <gpoladejesus@gmail.com>
2025-08-13 21:39:02 -0300

    feat sonner

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/react-query";
+import { Toaster } from "sonner";
 
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      <Toaster richColors />
       <RouterProvider router={router} />
     </QueryClientProvider>
   );
